bf5f4b81895b0b61da2e4ea11289f07cc5aefb0a
ShaymaTuriaqe <s12324759@stu.najah.edu>
2025-05-18 14:11:30 -0700

    notification & reviews tables controllers and models and routes

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,20 @@ use App\Http\Controllers\LessonController;
 use App\Http\Controllers\ReportController;
 use App\Http\Controllers\CertificateController;
 use App\Http\Controllers\WishlistController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\NotificationController;
+
+Route::resource('reviews', ReviewController::class);
+Route::get('notification', [NotificationController::class, 'index'])->name('notification.index');
+Route::get('notification/create', [NotificationController::class, 'create'])->name('notification.create');
+Route::post('notification', [NotificationController::class, 'store'])->name('notification.store');
+Route::get('notification/{notification}', [NotificationController::class, 'show'])->name('notification.show');
+Route::get('notification/{notification}/edit', [NotificationController::class, 'edit'])->name('notification.edit');
+Route::put('notification/{notification}', [NotificationController::class, 'update'])->name('notification.update');
+Route::delete('notification/{notification}', [NotificationController::class, 'destroy'])->name('notification.destroy');
 Route::resource('lesson', LessonController::class);
 Route::resource('dataEnrollments', EnrollmentsController::class);
-
 Route::resource('certificates', CertificateController::class);
-
 Route::get('/wishlist', [WishlistController::class, 'index'])->name('wishlist.index');
 Route::post('/wishlist', [WishlistController::class, 'store'])->name('wishlist.store');
 Route::delete('/wishlist/{id}', [WishlistController::class, 'destroy'])->name('wishlist.destroy');
