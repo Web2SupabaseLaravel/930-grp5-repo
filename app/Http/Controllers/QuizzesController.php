fba2a7e40a319c74f1eb88295633f130e388edd2
Izz961 <ez96zwana@gmail.com>
2025-05-26 02:17:27 +0300

    delete quizzes table

--- app/Http/Controllers/QuizzesController.php
+++ app/Http/Controllers/QuizzesController.php
@@ -7,31 +7,38 @@ use App\Models\Quizzes;
 
 class QuizzesController extends Controller
 {
-    public function create()
+    // عرض جميع الكويزات
+    public function index()
     {
-        $data['Quizzes'] = new Quizzes(); 
-        $data['route'] = 'dataQuizzes.store'; 
-        $data['method'] = 'post';
-        $data['titleForm'] = 'Form Input Quizzes'; 
-        $data['submitButton'] = 'Submit';
-
-        return view('Quizzes.form_Quizzes', $data); 
+        $quizzes = Quizzes::all();
+return view('quizzes.index', compact('quizzes'));
     }
 
+public function create()
+{
+    $data['quizzes'] = new Quizzes(); 
+$data['route'] = 'dataQuizzes.store';
+    $data['method'] = 'post';
+    $data['titleForm'] = 'Form Input Quizzes'; 
+    $data['submitButton'] = 'Submit';
+
+return view('quizzes.form_Quizzes', $data);
+}
+
+    // حفظ بيانات الكويز في قاعدة البيانات
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required',
-            'quiz_date' => 'required', 
-            'quiz_topic' => 'required', 
+            'name' => 'required|string|max:255',
+            'quiz_date' => 'required|date', 
+            'quiz_topic' => 'required|string|max:255', 
         ]);
 
         $inputQuizzes = new Quizzes(); 
         $inputQuizzes->name = $request->name;
-        $inputQuizzes->quiz_date = $request->quiz_date;  
         $inputQuizzes->quiz_topic = $request->quiz_topic; 
         $inputQuizzes->save();
 
-        return redirect('dataQuizzes/create'); 
+return redirect()->route('dataQuizzes.create')->with('success', 'Quiz created successfully!');
     }
 }
